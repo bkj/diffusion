94bf1779e6231abe93cd9ca9d3b336bb33a11659
Ben Johnson <bkj.322@gmail.com>
2019-09-12 19:06:26 -0400

    ...

--- diffusion.py
+++ diffusion.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-" diffusion module "
-
 import os
 import time
 import numpy as np
@@ -14,75 +12,82 @@ from tqdm import tqdm
 from knn import KNN, ANN
 
 
-trunc_ids = None
+trunc_ids  = None
 trunc_init = None
-lap_alpha = None
+lap_alpha  = None
 
 
 def get_offline_result(i):
-    ids = trunc_ids[i]
+    ids       = trunc_ids[i]
     trunc_lap = lap_alpha[ids][:, ids]
     scores, _ = linalg.cg(trunc_lap, trunc_init, tol=1e-6, maxiter=20)
     return scores
 
 
-def cache(filename):
-    """Decorator to cache results
-    """
-    def decorator(func):
-        def wrapper(*args, **kw):
-            self = args[0]
-            path = os.path.join(self.cache_dir, filename)
-            time0 = time.time()
-            if os.path.exists(path):
-                result = joblib.load(path)
-                cost = time.time() - time0
-                print('[cache] loading {} costs {:.2f}s'.format(path, cost))
-                return result
-            result = func(*args, **kw)
-            cost = time.time() - time0
-            print('[cache] obtaining {} costs {:.2f}s'.format(path, cost))
-            joblib.dump(result, path)
-            return result
-        return wrapper
-    return decorator
+# def cache(filename):
+#     """Decorator to cache results
+#     """
+#     def decorator(func):
+#         def wrapper(*args, **kw):
+#             self = args[0]
+#             path = os.path.join(self.cache_dir, filename)
+#             time0 = time.time()
+#             if os.path.exists(path):
+#                 result = joblib.load(path)
+#                 cost = time.time() - time0
+#                 print('[cache] loading {} costs {:.2f}s'.format(path, cost))
+#                 return result
+#             result = func(*args, **kw)
+#             cost = time.time() - time0
+#             print('[cache] obtaining {} costs {:.2f}s'.format(path, cost))
+#             joblib.dump(result, path)
+#             return result
+#         return wrapper
+#     return decorator
 
 
 class Diffusion(object):
     """Diffusion class
     """
     def __init__(self, features, cache_dir):
-        self.features = features
-        self.N = len(self.features)
+        self.features  = features
+        self.N         = len(self.features)
         self.cache_dir = cache_dir
-        # use ANN for large datasets
+        
         self.use_ann = self.N >= 100000
         if self.use_ann:
             self.ann = ANN(self.features, method='cosine')
+        
         self.knn = KNN(self.features, method='cosine')
-
-    @cache('offline.jbl')
+        
+    # @cache('offline.jbl')
     def get_offline_results(self, n_trunc, kd=50):
         """Get offline diffusion results for each gallery feature
         """
         print('[offline] starting offline diffusion')
         print('[offline] 1) prepare Laplacian and initial state')
+        
         global trunc_ids, trunc_init, lap_alpha
+        
         if self.use_ann:
+            # raise Exception
             _, trunc_ids = self.ann.search(self.features, n_trunc)
             sims, ids = self.knn.search(self.features, kd)
             lap_alpha = self.get_laplacian(sims, ids)
         else:
             sims, ids = self.knn.search(self.features, n_trunc)
             trunc_ids = ids
-            lap_alpha = self.get_laplacian(sims[:, :kd], ids[:, :kd])
+            aff       = self.get_affinity(sims=sims[:, :kd], ids=ids[:, :kd])
+            lap_alpha = self.get_laplacian(sims=sims[:, :kd], ids=ids[:, :kd])
+        
         trunc_init = np.zeros(n_trunc)
         trunc_init[0] = 1
-
+        
         print('[offline] 2) gallery-side diffusion')
         results = Parallel(n_jobs=-1, prefer='threads')(delayed(get_offline_result)(i)
                                       for i in tqdm(range(self.N),
                                                     desc='[offline] diffusion'))
+        
         all_scores = np.concatenate(results)
 
         print('[offline] 3) merge offline results')
@@ -94,17 +99,18 @@ class Diffusion(object):
 
     # @cache('laplacian.jbl')
     def get_laplacian(self, sims, ids, alpha=0.99):
-        """Get Laplacian_alpha matrix
-        """
+        """Get Laplacian_alpha matrix """
+        
         affinity = self.get_affinity(sims, ids)
-        num = affinity.shape[0]
-        degrees = affinity @ np.ones(num) + 1e-12
+        num      = affinity.shape[0]
+        degrees  = affinity @ np.ones(num) + 1e-12
+        
         # mat: degree matrix ^ (-1/2)
-        mat = sparse.dia_matrix(
-            (degrees ** (-0.5), [0]), shape=(num, num), dtype=np.float32)
+        mat = sparse.dia_matrix((degrees ** (-0.5), [0]), shape=(num, num), dtype=np.float32)
+        
         stochastic = mat @ affinity @ mat
-        sparse_eye = sparse.dia_matrix(
-            (np.ones(num), [0]), shape=(num, num), dtype=np.float32)
+        sparse_eye = sparse.dia_matrix((np.ones(num), [0]), shape=(num, num), dtype=np.float32)
+        
         lap_alpha = sparse_eye - alpha * stochastic
         return lap_alpha
 
@@ -117,12 +123,16 @@ class Diffusion(object):
         Returns:
             affinity: affinity matrix
         """
-        num = sims.shape[0]
+        
+        num            = sims.shape[0]
         sims[sims < 0] = 0  # similarity should be non-negative
+        
         sims = sims ** gamma
-        # vec_ids: feature vectors' ids
-        # mut_ids: mutual (reciprocal) nearest neighbors' ids
+        
+        # vec_ids:  feature vectors' ids
+        # mut_ids:  mutual (reciprocal) nearest neighbors' ids
         # mut_sims: similarites between feature vectors and their mutual nearest neighbors
+        
         vec_ids, mut_ids, mut_sims = [], [], []
         for i in range(num):
             # check reciprocity: i is in j's kNN and j is in i's kNN
@@ -131,8 +141,30 @@ class Diffusion(object):
                 vec_ids.append(i * np.ones(ismutual.sum(), dtype=int))
                 mut_ids.append(ids[i, ismutual])
                 mut_sims.append(sims[i, ismutual])
+        
         vec_ids, mut_ids, mut_sims = map(np.concatenate, [vec_ids, mut_ids, mut_sims])
         affinity = sparse.csc_matrix((mut_sims, (vec_ids, mut_ids)),
                                      shape=(num, num), dtype=np.float32)
+        
         affinity[range(num), range(num)] = 0
         return affinity
+
+# # >>
+# # Alternative `get_affinity`
+
+# from scipy import sparse
+
+# s = sims[:, :kd] ** gamma
+# i = ids[:, :kd]
+
+# row = np.repeat(np.arange(s.shape[0]), s.shape[1])
+# col = np.ravel(i)
+# val = np.ravel(s)
+
+# m = sparse.csc_matrix((val, (row, col)))
+# m.setdiag(0)
+# m = m.minimum(m.T)
+# m.eliminate_zeros()
+# m.sort_indices()
+# # <<
+
---
+++ run.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# run.sh
+
+# --
+# Setup env
+
+conda create -n diffusion_env python==3.7 pip -y
+conda activate diffusion_env
+
+# --
+# Install dependencies
+
+conda install -y -c pytorch faiss-cpu==1.5.3
+conda install -y joblib==0.13.2
+conda install -y tqdm==4.35.0
+conda install -y h5py==2.10.5
+conda install -y scipy==1.3.1
+conda install -y scikit-learn==0.21.3
+
+# --
+# Download data
+
+wget http://cmp.felk.cvut.cz/cnnimageretrieval/data/test/oxford5k/gnd_oxford5k.pkl \
+    -O data/gnd_oxford5k.pkl
+
+wget http://cmp.felk.cvut.cz/cnnimageretrieval/data/test/paris6k/gnd_paris6k.pkl \
+    -O data/gnd_paris6k.pkl
+
+cd data
+ln -s gnd_oxford5k.pkl gnd_oxford105k.pkl
+ln -s gnd_paris6k.pkl gnd_paris106k.pkl
+cd ..
+
+for dataset in oxford5k paris6k; do
+    for feature in siamac resnet; do
+        wget ftp://ftp.irisa.fr/local/texmex/corpus/diffusion/data/${dataset}_${feature}.mat \
+            -O data/${dataset}_${feature}.mat
+    done
+done
+
+# --
+# Convert matlab to python
+
+python mat2npy.py --dataset_name oxford5k --feature_type resnet --mat_dir data
+python mat2npy.py --dataset_name paris6k --feature_type resnet --mat_dir data
+
+# --
+# Run
+
+python rank.py                                                \
+    --cache_dir        tmp/oxford5k_resnet                    \
+    --query_path       data/query/oxford5k_resnet_glob.npy    \
+    --gallery_path     data/gallery/oxford5k_resnet_glob.npy  \
+    --gnd_path         data/gnd_oxford5k.pkl                  \
+    --dataset_name     oxford5k                               \
+    --truncation_size  1000
+
