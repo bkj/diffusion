1f3a8868aa2601315f7bd4cd5cdec618d73171c5
ezekielbarnett <ezekbarnett@gmail.com>
2019-11-22 18:07:26 +0100

    small updates on weighted average / point adding based on entropy scheme

--- scratch/active-learning-extension.py
+++ scratch/active-learning-extension.py
@@ -8,14 +8,13 @@
         python scratch/active-learning-extension.py --data_dir DATA/DIRECTORY --prob-name cifar10 --ts false
 
 """
-
-
 import sys
 sys.path.append('.')
 
 import argparse
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import normalize
 from sklearn_extra.cluster import KMedoids
@@ -38,6 +37,7 @@ def weighted_KMedioids(X, y, metric_fn, n_clusters=10, max_iter=1000, n_weights=
         true_classes = y[train_idx]
         guess_entropy = np.zeros((n,))
 
+        #Method for both computing the labels based on weights, plus adding points based on entropy.
         if weighted_point:
             y_hat = []
             pred_idx = true_classes[pred_max]
@@ -52,6 +52,7 @@ def weighted_KMedioids(X, y, metric_fn, n_clusters=10, max_iter=1000, n_weights=
                 guess_entropy[i] = entr(guess).sum()
                 y_hat.append(row_unique[guess.argmax()])
 
+        #Method for adding points based only on entropy, not considering multiple signals from same label.
         else:
             pred_idx = orig_scores[:, train_idx].argmax(axis=-1)
             y_hat = y[train_idx][pred_idx]
@@ -62,8 +63,9 @@ def weighted_KMedioids(X, y, metric_fn, n_clusters=10, max_iter=1000, n_weights=
         print('KMedioid function, accuracy after iter ', iter, ':', metric_fn(y, y_hat))
         highest_entropy = np.argsort(guess_entropy)[::-1][:n_entropy]
         train_idx = np.append(train_idx, highest_entropy)
+        acc = metric_fn(y, y_hat)
 
-    return y_hat
+    return y_hat, acc
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -71,6 +73,7 @@ def parse_args():
     parser.add_argument('--data_dir', type=str)
     parser.add_argument('--seed', type=int, default=123)
     parser.add_argument('--ts', type=str, default='false')
+    parser.add_argument('--results', type=str)
     return parser.parse_args()
 
 args = parse_args()
@@ -94,13 +97,13 @@ else:
     X_test = np.load(f'{base_dir}/features/{dataset}/0/test/x.npy')
     y_test = np.load(f'{base_dir}/features/{dataset}/0/test/y.npy')
 
-p = np.random.permutation(X_train.shape[0])
 
-#X_train, y_train = permute_data(X_train, y_train)
-X_train = X_train[p]
-y_train = y_train[p]
 X_test = normalize(X_test, 'l2', axis=1)
 X_train = normalize(X_train, 'l2', axis=1)
+
+#For some reason this is not working for X_train/y_train
+p = np.random.permutation(X_train.shape[0])
+X_train, y_train = X_train[p], y_train[p]
 X_test, y_test   = permute_data(X_test, y_test)
 X                = np.vstack([X_train, X_test])
 y                = np.hstack([y_train, y_test])
@@ -120,62 +123,73 @@ cos_dists = squareform(pdist(X, metric='cosine'))
 np.fill_diagonal(scores, np.inf)
 
 # Testcases
-props = .4
-k = int(props*n_samples)
-n_weights = 5
-n_entropy = 10
-entropy_iters = 1
+results = []
+props =[.1, .2, .3, .4, .5, .6]
+for prop in props:
 
-print("WEIGHTED POINTS")
-y_hat = weighted_KMedioids(X=orig_scores,
-                           y=y,
-                           metric_fn=metric_fn,
-                           n_clusters=k,
-                           max_iter=1000,
-                           n_weights=n_weights,
-                           entropy_iters=entropy_iters,
-                           n_entropy=n_entropy,
-                           weighted_point=True)
+    k = int(prop*n_samples)
+    n_weights = 5
+    n_entropy = 5
+    entropy_iters = 20
 
-print("UNWEIGHTED POINTS")
-y_hat = weighted_KMedioids(X=orig_scores,
-                           y=y,
-                           metric_fn=metric_fn,
-                           n_clusters=k,
-                           max_iter=1000,
-                           n_weights=n_weights,
-                           entropy_iters=entropy_iters,
-                           n_entropy=n_entropy,
-                           weighted_point=False)
+    print("WEIGHTED POINTS")
+    y_hat, acc_weighted = weighted_KMedioids(X=orig_scores,
+                               y=y,
+                               metric_fn=metric_fn,
+                               n_clusters=k,
+                               max_iter=1000,
+                               n_weights=n_weights,
+                               entropy_iters=entropy_iters,
+                               n_entropy=n_entropy,
+                               weighted_point=True)
 
-
-meds = min(n_samples, k + (n_entropy * entropy_iters))
-
-print("equivalent points:", meds)
-
-kmed = KMedoids(n_clusters=meds, metric='precomputed', max_iter=5000)
-train = kmed.fit(orig_scores.max() - orig_scores).medoid_indices_
-pred_idx = orig_scores[:, train].argmax(axis=-1)
-y_guess_kmed = y[train][pred_idx]
-print('kmedoid, (diff)', metric_fn(y, y_guess_kmed))
-
-kmed = KMedoids(n_clusters=meds, metric='precomputed', max_iter=5000)
-train = kmed.fit(orig_scores.max() - orig_scores).medoid_indices_
-pred_idx = orig_scores[:, train].argmax(axis=-1)
-y_guess_kmed = y[train][pred_idx]
-print('kmedoid, (diff) + n_entropy', metric_fn(y, y_guess_kmed))
-
-#SVC
-model = svm.LinearSVC().fit(X[train,:], y[train])
-pred = model.predict(X_test)
-print('SVC Linear', metric_fn(y_test, pred))
-
-#RBF
-model2 = svm.SVC(gamma='scale', decision_function_shape='ovo', kernel='rbf').fit(X[train,:], y[train])
-pred2 = model2.predict(X_test)
-print('SVC Kernel', metric_fn(y_test, pred2))
+    print("UNWEIGHTED POINTS")
+    y_hat, acc_unweighted = weighted_KMedioids(X=orig_scores,
+                               y=y,
+                               metric_fn=metric_fn,
+                               n_clusters=k,
+                               max_iter=1000,
+                               n_weights=n_weights,
+                               entropy_iters=entropy_iters,
+                               n_entropy=n_entropy,
+                               weighted_point=False)
 
 
+    meds = min(n_samples, k + (n_entropy * entropy_iters))
 
+    print("equivalent points:", meds)
 
+    kmed = KMedoids(n_clusters=k, metric='precomputed', max_iter=5000)
+    train = kmed.fit(orig_scores.max() - orig_scores).medoid_indices_
+    pred_idx = orig_scores[:, train].argmax(axis=-1)
+    y_guess_kmed = y[train][pred_idx]
+    acc_kmedioid_k = metric_fn(y, y_guess_kmed)
+    print('kmedoid, (diff)', acc_kmedioid_k)
+
+    kmed = KMedoids(n_clusters=meds, metric='precomputed', max_iter=5000)
+    train = kmed.fit(orig_scores.max() - orig_scores).medoid_indices_
+    pred_idx = orig_scores[:, train].argmax(axis=-1)
+    y_guess_kmed = y[train][pred_idx]
+    acc_kmedioid_kplus = metric_fn(y, y_guess_kmed)
+    print('kmedoid, (diff)', acc_kmedioid_kplus)
+
+    #SVC (indexing only kmedioids train points may be incorrect?)
+    model = svm.LinearSVC().fit(X[train,:], y[train])
+    pred = model.predict(X_test)
+    acc_svc = metric_fn(y_test, pred)
+    print('SVC Linear', acc_svc)
+
+    #RBF (indexing only kmedioids train points may be incorrect?)
+    model2 = svm.SVC(gamma='scale', decision_function_shape='ovo', kernel='rbf').fit(X[train,:], y[train])
+    pred2 = model2.predict(X_test)
+    acc_rbf = metric_fn(y_test, pred2)
+    print('SVC Kernel', acc_rbf)
+
+    results.append([prop, acc_weighted, acc_unweighted, acc_kmedioid_k, acc_kmedioid_kplus, acc_svc, acc_rbf])
+
+df = pd.DataFrame(results, columns=['prop', 'acc_weighted', 'acc_unweighted', 'acc_kmedioid_k', 'acc_kmediod_kplus', 'acc_svc', 'acc_rbf'])
+
+df.plot(kind='line', x='prop', ylim=[0,1.1], title='accuracy {}'.format(args.prob_name), legend=True)
+plt.show()
+#plt.savefig(f'/{args.results}/{args.prob_name}.png')
 
