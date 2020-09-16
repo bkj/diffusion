ca11cd515d4eb3ba4f3261f74c2c7c86eb4a940d
Ben Johnson <bkj.322@gmail.com>
2020-09-15 23:28:37 -0400

    cleaning up

--- diffusion.py
+++ diffusion.py
@@ -19,9 +19,14 @@ def parmap(f, x, n_jobs):
     jobs = [delayed(f)(xx) for xx in x]
     return Parallel(n_jobs=n_jobs, backend='multiprocessing')(jobs)
 
+sparse_sym_fns = {
+    "minimum" : lambda x: x.minimum(x.T).tocsr(),
+    # "maximum" : lambda x: x.maximum(x.T).tocsr(),
+    # "mean"    : lambda x: ((x + x.T) / 2).tocsr(),
+}
 
 class Diffusion(object):
-    def __init__(self, features, alpha=0.99, gamma=3, kd=50, n_jobs=60):
+    def __init__(self, features, alpha=0.99, gamma=3, kd=50, n_jobs=60, metric='cosine', sym_fn='minimum'):
         self.features    = features
         self.n_obs       = self.features.shape[0]
         self.feature_dim = self.features.shape[1]
@@ -31,8 +36,11 @@ class Diffusion(object):
         self.kd     = kd
         self.n_jobs = n_jobs
         
+        assert (np.abs((features ** 2).sum(axis=-1) - 1) < 1e-5).all(), 'Normalize features?'
         self.knn = faiss.IndexFlatIP(self.feature_dim)
         self.knn.add(features)
+        
+        self.sym_fn = sparse_sym_fns[sym_fn]
     
     def run(self, n_trunc, do_norm):
         global _fn
@@ -42,10 +50,10 @@ class Diffusion(object):
         # Make kd-nearest-neighbors symmetric affinity graph
         neib_sim = ball_sims[:, :self.kd] ** self.gamma
         neib_ids = ball_ids[:, :self.kd]
-        aff      = self.get_sym_aff(s=neib_sim, i=neib_ids)
+        adj      = self._aff2adj(s=neib_sim, i=neib_ids)
         
         # Make laplacian
-        lap = self.get_laplacian(aff=aff)
+        lap = self._adj2lap(adj=adj)
         
         # Initial signal (query is always most similar to self, so always at index 0)
         signal    = np.zeros(n_trunc)
@@ -70,25 +78,28 @@ class Diffusion(object):
             out = normalize(out, norm="l2", axis=1)
         
         return out
-        
-    def get_laplacian(self, aff):
-        n  = aff.shape[0]
-        
-        D = aff @ np.ones(n) + 1e-12
-        D = D ** (-0.5)
-        D = sparse.diags(D)
-        
-        return sparse.eye(n) - self.alpha * (D @ aff @ D)
     
-    def get_sym_aff(self, s, i):
+    def _aff2adj(self, s, i):
         row = np.repeat(np.arange(s.shape[0]), s.shape[1])
         col = np.ravel(i)
         val = np.ravel(s)
         aff = sparse.csc_matrix((val, (row, col)))
         
-        aff.setdiag(0)           # Remove self-similarity
-        aff = aff.minimum(aff.T) # Make symmetric
+        aff.setdiag(0)         # Remove self-similarity
+        
+        aff = self.sym_fn(aff) # Make symmetric
         aff.eliminate_zeros()
         aff.sort_indices()
         
         return aff
+    
+    def _adj2lap(self, adj):
+        n = adj.shape[0]
+        
+        D = adj @ np.ones(n) + 1e-12
+        D = D ** (-0.5)
+        D = sparse.diags(D)
+        
+        DAD = (D @ adj @ D)
+        
+        return sparse.eye(n) - self.alpha * DAD
